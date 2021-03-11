1b73cf5e6739b2c231f13d2175f2912133583064
stephan du Plooy <sdpphotography1@gmail.com>
2021-03-10 21:15:44 -0500

    Updated HTML, Added in first bits of code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Stephan-s-Multiple-Choice-Quiz-Do-YOU-know-what-you-are-talking-about-
+
---
+++ script.js
@@ -0,0 +1,7 @@
+var StartButton = document.querySelector(".startquiz");
+
+
+StartButton.addEventListener("click", startQuiz)
+
+function startQuiz()
+
